275728c75b2d3ff323f2edc225e2ce01c236a09a
IlanDeVinci <145458113+IlanDeVinci@users.noreply.github.com>
2024-03-13 00:19:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,6 @@
 "use strict"
 
-
-
-/*const cardData = [
-    {
-        image: 'images/harry-potter-film-famille-large.jpg',
-        title: 'Card 1',
-        description: 'Description for Card 1.',
-    },
-    {
-        image: 'images/hermione_granger__hbp_promo__5.webp',
-        title: 'Card 2',
-        description: 'Description for Card 2.',
-    },
-    {
-        image: 'images/Ron_Weasley_poster.jpg',
-        title: 'Card 1',
-        description: 'Description for Card 1.',
-    },
-    {
-        image: 'images/Voldemort_Infobox.webp',
-        title: 'Card 2',
-        description: 'Description for Card 2.',
-    },    {
-        image: 'images/harry-potter-film-famille-large.jpg',
-        title: 'Card 1',
-        description: 'Description for Card 1.',
-    },
-    {
-        image: 'images/hermione_granger__hbp_promo__5.webp',
-        title: 'Card 2',
-        description: 'Description for Card 2.',
-    },
-    {
-        image: 'images/Ron_Weasley_poster.jpg',
-        title: 'Card 1',
-        description: 'Description for Card 1.',
-    },
-    {
-        image: 'images/Voldemort_Infobox.webp',
-        title: 'Card 2',
-        description: 'Description for Card 2.',
-    },
-
-];
-
-function createCard(card) {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-    const imgcardElement = document.createElement('div');
-    imgcardElement.classList.add('imgcard');
-
-
-    const imageElement = document.createElement('img');
-    imageElement.classList.add('cardimg');
-    imageElement.src = card.image;
-    imageElement.alt = card.title;
-
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = card.title;
-
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = card.description;
-
-    cardElement.appendChild(imgcardElement);
-    imgcardElement.appendChild(imageElement);
-    cardElement.appendChild(titleElement);
-    cardElement.appendChild(descriptionElement);
-
-    return cardElement;
-}
-*/
 const cardContainer = document.getElementById('cardContainer');
-
-/*
-// Function to initialize the cards
-function initializeCards() {
-
-    // Loop through the card data and create card elements
-    cardData.forEach(card => {
-        const cardElement = createCard(card);
-        cardContainer.appendChild(cardElement);
-    });
-}
-
-// Call the initializeCards function when the page loads
-window.onload = initializeCards;
-*/
 
 let modal = document.getElementById("modal");
 
@@ -123,7 +37,7 @@ let hpCharacters = [];
 let hpList = [];
 let userList = [];
 const loadCharacters = async () => {
-        const res = await fetch('https://hp-api.onrender.com/api/characters');
+        const res = await fetch('https://hp-api.lainocs.fr/characters');
         hpCharacters = await res.json();
         hpCharacters.forEach(element => {
             if(element.image){
